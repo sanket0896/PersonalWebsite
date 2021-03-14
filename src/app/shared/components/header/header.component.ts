ea4777bff93de5eb54458c82a9096988bc749379
Sanket Mishra <sanket@spotdraft.com>
2021-03-15 01:44:03 +0530

    Refactor: Moved shared components into a folder

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,7 +2,9 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-header",
-  templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.scss"],
+  template: `
+    <app-desktop-header></app-desktop-header>
+    <app-mobile-header></app-mobile-header>
+  `,
 })
 export class HeaderComponent {}
--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./header.component";
-import { LogoModule } from "../logo/logo.module";
 import { MobileHeaderComponent } from "./components/mobile-header/mobile-header.component";
 import { DesktopHeaderComponent } from "./components/desktop-header/desktop-header.component";
+import { LogoModule } from "../logo/logo.module";
 import { LinksModule } from "../links/links.module";
 
 @NgModule({
