f0e0033027c46331881f0613e8404b6c317ab069
Sanket Mishra <sanket@spotdraft.com>
2021-03-14 13:42:41 +0530

    Setup routing, Added barebone components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core"
+import { Routes, RouterModule } from "@angular/router"
+import { BlogComponent } from "./blog/blog.component"
+import { LandingComponent } from "./landing/landing.component"
+import { ProjectsComponent } from "./projects/projects.component"
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LandingComponent,
+  },
+  {
+    path: "projects",
+    component: ProjectsComponent,
+  },
+  {
+    path: "blog",
+    component: BlogComponent,
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/landing/landing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingComponent } from './landing/landing.component';
+import { BlogComponent } from './blog/blog.component';
+
+
+
+@NgModule({
+  declarations: [LandingComponent, BlogComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LandingModule { }
