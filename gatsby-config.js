a470d1a9b9bccd5ccd2a17bf9fd4835583c42c8e
Sanket Mishra <sanket@spotdraft.com>
2021-03-14 13:16:53 +0530

    angular setup

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [
-        `gatsby-plugin-styled-components`,
-        // `gatsby-plugin-react-helmet`,
-        // `gatsby-transformer-sharp`,
-        // `gatsby-plugin-sharp`
-    ],
-}
