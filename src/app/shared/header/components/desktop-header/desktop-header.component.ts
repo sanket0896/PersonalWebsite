d65b04ebf50182ba4f3105f9f2d9a273ef81058a
Sanket Mishra <sanket@spotdraft.com>
2021-03-14 18:46:14 +0530

    Added media queries

---
+++ src/app/shared/header/components/desktop-header/desktop-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-desktop-header",
+  templateUrl: "./desktop-header.component.html",
+  styleUrls: ["./desktop-header.component.scss"],
+})
+export class DesktopHeaderComponent {}
--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -4,9 +4,11 @@ import { HeaderComponent } from "./header.component";
 import { LogoModule } from "../logo/logo.module";
 import { NavLinkComponent } from "./components/nav-link/nav-link.component";
 import { RouterModule } from "@angular/router";
+import { MobileHeaderComponent } from './components/mobile-header/mobile-header.component';
+import { DesktopHeaderComponent } from './components/desktop-header/desktop-header.component';
 
 @NgModule({
-  declarations: [HeaderComponent, NavLinkComponent],
+  declarations: [HeaderComponent, NavLinkComponent, MobileHeaderComponent, DesktopHeaderComponent],
   imports: [CommonModule, RouterModule, LogoModule],
   exports: [HeaderComponent],
 })
