845e862958821ceea034a0f5e1d630ac98edf5b2
Sanket Mishra <sanket@spotdraft.com>
2022-02-12 13:21:04 +0530

    Show underline on the nav link for child routes as wel

--- src/app/shared/components/links/nav-link/nav-link.component.ts
+++ src/app/shared/components/links/nav-link/nav-link.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { IsActiveMatchOptions, Router } from "@angular/router";
 
 @Component({
   selector: "app-nav-link",
@@ -9,7 +10,27 @@ export class NavLinkComponent {
   @Input()
   link: string = "";
 
-  readonly routerLinkActiveOptions = {
-    exact: true,
+  readonly routerLinkActiveOptions: IsActiveMatchOptions = {
+    paths: "subset",
+    matrixParams: "ignored",
+    queryParams: "ignored",
+    fragment: "ignored",
   };
+
+  private get exactOrSubset(): "exact" | "subset" {
+    return this.link === "" ? "exact" : "subset";
+  }
+
+  get isActive() {
+    const opts: IsActiveMatchOptions = {
+      paths: this.exactOrSubset,
+      queryParams: this.exactOrSubset,
+      fragment: "ignored",
+      matrixParams: "ignored",
+    };
+
+    return this.router.isActive(this.link, opts);
+  }
+
+  constructor(private router: Router) {}
 }
