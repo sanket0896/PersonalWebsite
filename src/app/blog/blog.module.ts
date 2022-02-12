9fb788b523b6a754cdf0003e53b7483f60ecec95
Sanket Mishra <sanket@spotdraft.com>
2022-02-12 13:09:23 +0530

    setup scully and blog module

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -5,9 +5,19 @@ import { BlogRoutingModule } from "./blog-routing.module";
 import { BlogComponent } from "./blog.component";
 import { SimpleCardModule } from "../shared/components/simple-card/simple-card.module";
 import { BlogEntryComponent } from "./components/blog-entry/blog-entry.component";
+import { ScullyLibModule } from "@scullyio/ng-lib";
+import { BlogPostComponent } from "./components/blog-post/blog-post.component";
+import { BlogListComponent } from "./components/blog-list/blog-list.component";
+import { BlogService } from "./blog.service";
 
 @NgModule({
-  declarations: [BlogComponent, BlogEntryComponent],
-  imports: [CommonModule, BlogRoutingModule, SimpleCardModule],
+  declarations: [
+    BlogComponent,
+    BlogEntryComponent,
+    BlogPostComponent,
+    BlogListComponent,
+  ],
+  imports: [CommonModule, BlogRoutingModule, SimpleCardModule, ScullyLibModule],
+  providers: [BlogService],
 })
 export class BlogModule {}
