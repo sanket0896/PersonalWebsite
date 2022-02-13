0e8c62ececdbc5682ae9719a214f8a3291e55a82
Sanket Mishra <sanket@spotdraft.com>
2022-02-13 22:45:35 +0530

    setup prismJS to highlight code

--- src/app/blog/components/blog-post/blog-post.component.ts
+++ src/app/blog/components/blog-post/blog-post.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import {
+  Component,
+  ChangeDetectionStrategy,
+  AfterViewChecked,
+} from "@angular/core";
+import { CodeHighlightService } from "src/app/shared/services/code-highlighter/code-highlighter.service";
 
 @Component({
   selector: "app-blog-post",
@@ -6,8 +11,15 @@ import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
   styleUrls: ["./blog-post.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BlogPostComponent implements OnInit {
-  constructor() {}
+export class BlogPostComponent implements AfterViewChecked {
+  constructor(private codeHighlighter: CodeHighlightService) {}
 
-  ngOnInit(): void {}
+  ngAfterViewChecked() {
+    /**
+     * Scully does async update of the html content. We can't be guaranteed when the template html
+     * is rendered. So using highlightAll() inside ngAfterViewChecked instead on ngAfterViewInit.
+     * This won't affect much, as cd won't be running that frequently on this component.
+     */
+    this.codeHighlighter.highlightAll();
+  }
 }
